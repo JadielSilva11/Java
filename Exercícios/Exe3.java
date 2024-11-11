c557cc75e759bf135e583d9cec0dcfe1d63aef66
JadielSilva11 <jdielcunha@gmail.com>
2024-11-11 09:58:40 -0300

    Commit
    Push do exercício Exe3

---
+++ Exercícios/Exe3.java
@@ -0,0 +1,8 @@
+public class Exe3{
+	public static void main(String args[]){
+		double numeroDecimal = 12345.6789;
+		int numeroInteiro = (int) numeroDecimal;
+		System.out.println("Numero Decimal: "+ numeroDecimal);
+		System.out.println("Numero Inteiro: "+ numeroInteiro);
+	}
+}
