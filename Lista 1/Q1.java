c4c996dd31de9572e44ab4e09d424f916ab0566e
JadielSilva11 <jdielcunha@gmail.com>
2024-11-20 21:54:49 -0300

    Commit
    Push da questão Q1

---
+++ Lista 1/Q1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Q1{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int x;
+        double y;
+        x = scanner.nextInt();
+        y = scanner.nextDouble();
+
+        System.out.println("Soma como Double: "+ (x + y));
+        System.out.println("Soma como Inteiro: "+ (int) (x + y));
+    }
+}
