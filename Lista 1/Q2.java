c35a472eff384f14ec0a2d49560e0ac93b4cc742
JadielSilva11 <jdielcunha@gmail.com>
2024-11-20 22:29:10 -0300

    Commit
    Push da questão Q2

---
+++ Lista 1/Q2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Q2 {
+    public static void main(String args[]){
+            Scanner scanner = new Scanner(System.in);
+            double x[] = new double[4];
+            double media = 0;
+            for(int i=0;i<4;i++)
+            {
+                System.out.println("Digite a nota "+ (i + 1));
+                x[i] = scanner.nextDouble();
+                media += x[i];
+            }
+
+            media /= 4;
+            
+            if(media >= 7)
+            {
+                System.out.println("Aprovado!!");
+                System.out.println("Sua media: "+ media);
+            }
+            else if(media < 7 && media >= 5)
+            {
+                System.out.println("Voce esta de racuperacao!!");
+                System.out.println("Sua media: "+ media);
+            }
+            else if(media < 5)
+            {
+                System.out.println("Voce esta reprovado!!");
+                System.out.println("Sua media: "+media);
+            }
+    }
+}
