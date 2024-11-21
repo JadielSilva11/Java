b400a25b04bcac16faa76f9818298975608a479c
JadielSilva11 <jdielcunha@gmail.com>
2024-11-20 22:30:14 -0300

    Commit
    Push da questão Exe5 da lista de exercicios (Questão não terminada)

--- Exercícios/Exe5.java
+++ Exercícios/Exe5.java
@@ -1,9 +1,9 @@
 public class Exe5{
-	public static void main([String args[]){
+	public static void main(String args[]){
 		boolean operandoA;
 		boolean operandoB;
 
-		\\ TABELA DO AND
+		// TABELA DO AND
 		System.out.println("");
 	}
 }
