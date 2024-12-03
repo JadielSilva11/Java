cdea3fb3e9ae6f1d82b43b2a35bd1b8aec6c08c0
JadielSilva11 <jdielcunha@gmail.com>
2024-12-02 22:16:53 -0300

    Commit
    Push do exercício Q3

---
+++ Lista 2/Q3.java
@@ -0,0 +1,13 @@
+public class Q3{
+	public static void main(String args[]){
+		int vetor[] = {2, 20, 15, 17, 81};
+
+		for(int i=0;i<5;i++)
+		{
+			if(vetor[i] == 81)
+			{
+				System.out.println("O numero 81 encontra-se na posicao do vetor: "+i);
+			}
+		}                                                                                                                                                                                                                                                                                                      
+	}
+}
