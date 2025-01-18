2b9d53262599f91475b5dd6bb53ef2b7c82da56d
JadielSilva11 <jdielcunha@gmail.com>
2025-01-18 20:39:34 -0300

    Commit
    Push do exercício Exe2

---
+++ Exercícios/Aula 2/Exe2.java
@@ -0,0 +1,8 @@
+public class Exe2{
+	public static void main(String args[]){
+		int idadeInteira = 25;
+		double idadeDecimal = idadeInteira;
+		System.out.println("Idade Inteira: "+ idadeInteira);
+		System.out.println("Idade Decimal: "+ idadeDecimal);
+	}
+}
