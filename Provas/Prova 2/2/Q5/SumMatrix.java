8945a0e7637628e9cf94b5e331c8910fa514fca8
JadielSilva11 <jdielcunha@gmail.com>
2025-02-03 22:55:32 -0300

    Commit
    Push da classe SumMatrix na questão 5 da prova 2

---
+++ Provas/Prova 2/2/Q5/SumMatrix.java
@@ -0,0 +1,7 @@
+public class SumMatrix implements MatrixOperations{
+	public int[][] performOperation(int m1[], int m2[][]){
+		x = m1.length;
+		y = m2.length;
+		int sumMat[][] = new int [x][y];
+	}
+}
