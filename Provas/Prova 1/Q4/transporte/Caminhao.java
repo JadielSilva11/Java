841acfd1334fe319f3531e884f089cb9571582df
JadielSilva11 <jdielcunha@gmail.com>
2025-02-03 22:32:58 -0300

    Commit
    Push da classe caminhão da questão 4

---
+++ Provas/Prova 1/Q4/transporte/Caminhao.java
@@ -0,0 +1,26 @@
+public class Caminhao{
+	private String marca;
+	private int ano;
+	private double quilometragem;
+
+	public void setMarca(String a){
+		marca = a;
+	}
+	public String getMarca(){
+		return marca;
+	}
+
+	public void setAno(int b){
+		ano = b;
+	}
+	public int getAno(){
+		return ano;
+	}
+
+	public void setQuilometragem(double c){
+		quilometragem = c;
+	}
+	public double getQuiçometragem(){
+		return quilometragem;
+	}
+}
